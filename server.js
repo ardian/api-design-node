7cc0acbbd2bdbbdf4bedd14597e2e468460bbb27
Ardian Haxha <ardian@brainpress.org>
2016-01-30 00:09:55 +0100

    Adding root page, and jsonData

--- server.js
+++ server.js
@@ -2,4 +2,26 @@
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
 
+
+
+var express = require('express');
+var app = express();
 var jsonData = {count: 12, message: 'hey'};
+
+app.get('/', function(req, res){
+  // res.sendFile takes an absolute path to a file and
+  // sets the mime type based n the file extname
+  res.sendFile(__dirname + '/index.html', function(err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  })
+});
+
+app.get('/data', function(req, res) {
+  res.json(jsonData);
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
